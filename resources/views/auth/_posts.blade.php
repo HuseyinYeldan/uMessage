3af2d07336097366301f211593bcf9b5b657f985
Hüseyin Yeldan <yeldanhuseyin1@gmail.com>
2023-12-17 22:12:18 +0300

    Minor design changes and post edit delete menu fix

--- resources/views/auth/_posts.blade.php
+++ resources/views/auth/_posts.blade.php
@@ -1,5 +1,5 @@
 @foreach ($posts as $post)
-<div class="post flex w-full mt-8 mb-8 p-4 shadow-md duration-300 rounded hover:ring-2 relative hover:ring-purple-100">
+<div class="post flex w-full mt-8 mb-8 p-4 shadow-md border border-gray-200 duration-300 rounded hover:ring-2 relative hover:ring-purple-300">
     <img src="/storage/{{ $post->user->avatar }}" class="rounded-full w-12 h-12 aspect-square mr-4 flex-shrink-0" alt="">
 
     <div class="post-info flex justify-start flex-col">
@@ -8,13 +8,13 @@
         <p class="text-sm my-2 break-all">{{ $post->body }}</p>
         <span class="text-xs mt-2 flex items-center">
             <i class="fa-regular fa-heart text-xl mr-1 text-slate-700 duration-300 cursor-pointer hover:text-red-400" class="likeButton"></i> <span>23 likes</span>
-            <i class="fa-regular fa-comment text-xl ml-4 mr-1 text-slate-700 duration-300 cursor-pointer hover:text-purple-400" class="likeButton"></i> <span>3 comments</span>
+            <i class="fa-regular fa-comment text-xl ml-4 mr-1 text-slate-700 duration-300 cursor-pointer hover:text-purple-600" class="likeButton"></i> <span>3 comments</span>
         </span>
     </div>
 
     <div class="post-other absolute right-2">
-        <button class="profileOther w-8 h-8"><i class="fa-solid fa-ellipsis text-xl relative -z-20 "></i></button>
-        <div class="absolute hidden w-24 h-16 rounded flex-col bg-white shadow-md">
+        <button class="profileOther w-8 h-8 duration-300 hover:text-purple-600"><i class="fa-solid fa-ellipsis text-xl relative -z-20"></i></button>
+        <div class="absolute hidden w-24 h-16 rounded flex-col bg-white shadow-md after:w-4 after:h-4 after:bg-yellow-400 after:rounded after:absolute after:-top-2 after:left-2 after:rotate-45 after:-z-30">
             <a href="" class="flex rounded-t justify-center items-center flex-1 text-center duration-300 bg-yellow-400 font-bold text-xs text-yellow-900 hover:bg-yellow-500 ">Edit Post</a>
             <a href="" class="flex rounded-b justify-center items-center flex-1 text-center duration-300 bg-red-400 font-bold text-red-900 text-xs hover:bg-red-500">Delete Post</a>
         </div>
@@ -22,27 +22,39 @@
 </div>
 @endforeach
 
+
 <script>
-    let profileOther = document.getElementsByClassName('profileOther');
-    
-    for (let i = 0; i < profileOther.length; i++) {
+    let profileOther = document.querySelectorAll('.profileOther');
 
-        profileOther[i].addEventListener('click',function(e){
+    profileOther.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            const menu = e.currentTarget.nextElementSibling;
 
-            for (let j = 0; j < profileOther.length; j++) {
-                profileOther[j].parentNode.children[1].classList.remove('flex')
-                profileOther[j].parentNode.children[1].classList.add('hidden')
-            }
+            profileOther.forEach((otherButton) => {
+                const otherMenu = otherButton.nextElementSibling;
+                if (otherMenu !== menu) {
+                    otherMenu.classList.remove('flex');
+                    otherMenu.classList.add('hidden');
+                }
+            });
 
-            console.log(profileOther[i].parentNode.children[1]);
-            if(e.target.parentNode.children[1].classList.contains('hidden')){
-                e.target.parentNode.children[1].classList.remove('hidden')
-                e.target.parentNode.children[1].classList.add('flex')
-            }
-            else{
-                e.target.parentNode.children[1].classList.add('hidden')
-                e.target.parentNode.children[1].classList.remove('flex')
-            }
-        })
+            menu.classList.toggle('hidden');
+            menu.classList.toggle('flex');
+
+            // Add event listener to close the menu when clicking outside
+            document.addEventListener('click', closeMenuOutside);
+        });
+    });
+
+    function closeMenuOutside(e) {
+        if (!e.target.closest('.post-other')) {
+            profileOther.forEach((button) => {
+                const menu = button.nextElementSibling;
+                menu.classList.remove('flex');
+                menu.classList.add('hidden');
+            });
+
+            document.removeEventListener('click', closeMenuOutside);
+        }
     }
 </script>
