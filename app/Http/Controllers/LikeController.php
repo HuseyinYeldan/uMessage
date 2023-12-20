b5f22ce62f1eaeb81fead666602730e66b47c6bf
Hüseyin Yeldan <yeldanhuseyin1@gmail.com>
2023-12-20 15:23:00 +0300

    Typo fix

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -26,10 +26,10 @@ class LikeController extends Controller
             $data['isComment'] = (int)$request->input('isComment');
             
             Like::create($data);
-            return redirect()->back()->with('success','Post liked');
+            return redirect()->back()->with('success',$isComment?'Comment liked':'Post Liked');
         }
             $existingLike->delete();
-            return redirect()->back()->with('success','Post unliked');
+            return redirect()->back()->with('success',$isComment?'Comment unliked':'Post unliked');
 
     }
 }
